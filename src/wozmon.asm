8963a7ddbc07f1e00bc797488cc639a866a92f98
Rey <reyco2000@gmail.com>
2026-09-21 12:05:03 -0500

    feat: add PUTCHAR with CR handling and screen scroll

--- src/wozmon.asm
+++ src/wozmon.asm
@@ -40,4 +40,60 @@ ENTRY       orcc  #$50              ; mask IRQ and FIRQ, both builds
             endc
             rts                     ; replaced in Task 10
 
+CR          equ $0D
+VDGSPC      equ $20                 ; VDG code for space
+
+; --- PUTCHAR: print ASCII char in A. Preserves A, B, X, U. ---
+; The AND #$3F maps ASCII to the MC6847's 64-entry set, which begins at
+; '@'. Bit 6 is the inverse-video bit; its correct polarity is verification
+; item 1 in the spec and is confirmed on hardware in Task 12. If normal
+; text needs bit 6 set, change this single instruction to "ora #$40".
+PUTCHAR     pshs  a,b,x
+            cmpa  #CR
+            beq   PUTCR
+            ldx   <CURSOR
+            anda  #$3F
+            sta   ,x+
+            stx   <CURSOR
+            bra   PUTCHK
+PUTCR       ldd   <CURSOR
+            subd  #SCREEN
+            orb   #31               ; round up to end of this line
+            addd  #1                ; then step to the next line's start
+            addd  #SCREEN
+            std   <CURSOR
+PUTCHK      ldx   <CURSOR
+            cmpx  #SCREND
+            blo   PUTDONE
+            bsr   SCROLL
+PUTDONE     puls  a,b,x,pc
+
+; --- SCROLL: move rows 1-15 up one, blank the last, home cursor there ---
+SCROLL      pshs  a,x,u
+            ldx   #SCREEN+32
+            ldu   #SCREEN
+SCRLP       ldd   ,x++
+            std   ,u++
+            cmpx  #SCREND
+            blo   SCRLP
+            ldx   #SCREND-32
+            lda   #VDGSPC
+SCRBLK      sta   ,x+
+            cmpx  #SCREND
+            blo   SCRBLK
+            ldx   #SCREND-32
+            stx   <CURSOR
+            puls  a,x,u,pc
+
+; --- CLS: blank the screen and home the cursor ---
+CLS         pshs  a,x
+            ldx   #SCREEN
+            lda   #VDGSPC
+CLSLP       sta   ,x+
+            cmpx  #SCREND
+            blo   CLSLP
+            ldx   #SCREEN
+            stx   <CURSOR
+            puls  a,x,pc
+
             end ENTRY
